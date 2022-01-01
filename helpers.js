345416a3c1eebbee9acd84ba759298e6759023dd
Abyan Mohamed <abyanmohamed99@gmail.com>
2022-01-01 21:25:45 +0000

    Fixed the issue with rejection

--- helpers.js
+++ helpers.js
@@ -1,11 +1,48 @@
-const getUserByEmail = function(email, database) {
-    for(const user in database) {
-      if(database[user].email == email) {
-        return user;
-      } 
-    }
-    return "undefined"
-   
-  };
+const bcrypt = require('bcryptjs');
 
-  module.exports = {getUserByEmail};
+const generateRandomString = function () {
+  return Math.random().toString(36).slice(2, 8);
+};
+
+const getEmail = function (obj, str) {
+  for (const id in obj) {
+    if (obj[id].email === str) {
+      return true;
+    }
+  }
+};
+
+
+const emailPwdMatch = function (obj, email, pwd) {
+  for (const id in obj) {
+    if ((obj[id].email === email) && (bcrypt.compareSync(pwd, obj[id].password))) {
+      return true;
+    }
+  }
+};
+
+const getUserID = function (userObj, email) {
+  let user_id;
+  for (let user in userObj) {
+    let emails = userObj[user].email;
+    if (emails === email) {
+      user_id = userObj[user].id;
+    }
+  }
+  return user_id;
+};
+
+const urlsForUser = function (userID, databaseObj) {
+  let newUserObj = {};
+
+  for (const shortURL in databaseObj) {
+    let databaseUserID = databaseObj[shortURL].userID;
+
+    if (userID === databaseUserID) {
+      newUserObj[shortURL] = databaseObj[shortURL];
+    }
+  }
+  return newUserObj;
+};
+
+module.exports = { generateRandomString, getEmail, emailPwdMatch, getUserID, urlsForUser };
